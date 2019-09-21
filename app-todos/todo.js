7d3f532d23af14db0d1373be7de4eac553748ff7
Rogério <roger.lista@gmail.com>
2019-09-21 13:53:37 -0300

    App todo - renderizando Todos

--- app-todos/todo.js
+++ app-todos/todo.js
@@ -2,8 +2,20 @@ let listElement = document.querySelector('#app ul')
 let inputElement = document.querySelector('#app input')
 let buttonElement = document.querySelector('#app button')
 
-let lista = [
+let todos = [
   'Fazer café',
   'Estudar Javascript',
   'Entrar na comunidade da Rocketseat'
 ]
+
+function renderTodos() {
+  for (let todo of todos) {
+    let todoElement = document.createElement('li')
+    let textElement = document.createTextNode(todo)
+
+    todoElement.appendChild(textElement)
+    listElement.appendChild(todoElement)
+  }
+}
+
+renderTodos()
