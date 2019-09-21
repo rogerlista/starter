d11d11a6b3500926af8ff349cdbdefaeb6346a66
Rogério <roger.lista@gmail.com>
2019-09-21 13:47:27 -0300

    App todo - iniciando a aplicação

--- app-todos/todo.js
+++ app-todos/todo.js
@@ -1,1 +1,9 @@
-alert('Funcionou...')
+let listElement = document.querySelector('#app ul')
+let inputElement = document.querySelector('#app input')
+let buttonElement = document.querySelector('#app button')
+
+let lista = [
+  'Fazer café',
+  'Estudar Javascript',
+  'Entrar na comunidade da Rocketseat'
+]
