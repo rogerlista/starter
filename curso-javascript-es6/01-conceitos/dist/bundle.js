03a1ed1700e3c1e47e03c317efd6489d1c869c4c
Rogério <roger.lista@gmail.com>
2019-09-24 07:22:25 -0300

    Conceitos - valores padrão

--- curso-javascript-es6/01-conceitos/dist/bundle.js
+++ curso-javascript-es6/01-conceitos/dist/bundle.js
@@ -1,72 +1,29 @@
 "use strict";
 
-var arr = [1, 3, 4, 5, 8, 10]; // retorna um novo array fazendo alguma coisa com os elementos do array atual.
+function soma(a, b) {
+  return a + b;
+}
 
-var newArr = arr.map(function (item, index) {
-  return item + index;
-});
-console.log(newArr); // [1, 4, 6, 8, 12, 15]
-// retorna um único valor fazendo alguma coisa com os elementos do array atual.
+console.log(soma(1)); // NaN
 
-var sum = arr.reduce(function (total, next) {
-  return total + next;
-}); // não informando o valor inicial de total ele será 0 por padrão.
+console.log(soma()); // NaN
 
-console.log(sum); // 31
-// retorna um novo array retirando os elementos que forem falsos de acordo com a condição declarada.
+function somaComValoresPadrao() {
+  var a = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 3;
+  var b = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 6;
+  return a + b;
+}
 
-var filter = arr.filter(function (item) {
-  return item % 2 === 0;
-});
-console.log(filter); // [4, 8, 10]
-// retorna o elemento se encontrar ou indefined.
+console.log(somaComValoresPadrao(1)); // 7
 
-var find = arr.find(function (item) {
-  return item === 4;
-});
-console.log(find); // 4
+console.log(somaComValoresPadrao()); // 9
 
-var notFind = arr.find(function (item) {
-  return item === 2;
-});
-console.log(notFind); // indefined
-// NÃO RECOMENDADO
-
-var teste = function teste() {
-  return 'teste';
+var somaArrowFunction = function somaArrowFunction() {
+  var a = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 3;
+  var b = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 6;
+  return a + b;
 };
 
-console.log(teste()); // teste
+console.log(somaArrowFunction(1)); // 7
 
-var testeArray = function testeArray() {
-  return [1, 2, 3];
-};
-
-console.log(testeArray()); // [1, 2, 3]
-
-var testeString = function testeString() {
-  return 'Teste';
-};
-
-console.log(testeString()); // Teste
-
-var testeNumber = function testeNumber() {
-  return 3;
-};
-
-console.log(testeNumber()); // 3
-// não vai funcionar entende que seja o corpo da função
-
-var testeObjeto = function testeObjeto() {
-  nome: 'Juca';
-};
-
-console.log(testeObjeto()); // undefined
-
-var testeObjetoCerto = function testeObjetoCerto() {
-  return {
-    nome: 'Juca'
-  };
-};
-
-console.log(testeObjetoCerto()); // { nome: 'Juca' }
+console.log(somaArrowFunction()); // 9
--- curso-javascript-es6/01-conceitos/main.js
+++ curso-javascript-es6/01-conceitos/main.js
@@ -1,45 +1,18 @@
-const arr = [1, 3, 4, 5, 8, 10]
-
-// retorna um novo array fazendo alguma coisa com os elementos do array atual.
-const newArr = arr.map((item, index) => item + index)
-console.log(newArr) // [1, 4, 6, 8, 12, 15]
-
-// retorna um único valor fazendo alguma coisa com os elementos do array atual.
-const sum = arr.reduce((total, next) => total + next) // não informando o valor inicial de total ele será 0 por padrão.
-console.log(sum) // 31
-
-// retorna um novo array retirando os elementos que forem falsos de acordo com a condição declarada.
-const filter = arr.filter(item => item % 2 === 0)
-console.log(filter) // [4, 8, 10]
-
-// retorna o elemento se encontrar ou indefined.
-const find = arr.find(item => item === 4)
-console.log(find) // 4
-
-const notFind = arr.find(item => item === 2)
-console.log(notFind) // indefined
-
-// NÃO RECOMENDADO
-const teste = () => {
-  return 'teste'
+function soma(a, b) {
+  return a + b
 }
 
-console.log(teste()) // teste
+console.log(soma(1)) // NaN
+console.log(soma()) // NaN
 
-const testeArray = () => [1, 2, 3]
-console.log(testeArray()) // [1, 2, 3]
-
-const testeString = () => 'Teste'
-console.log(testeString()) // Teste
-
-const testeNumber = () => 3
-console.log(testeNumber()) // 3
-
-// não vai funcionar entende que seja o corpo da função
-const testeObjeto = () => {
-  nome: 'Juca'
+function somaComValoresPadrao(a = 3, b = 6) {
+  return a + b
 }
-console.log(testeObjeto()) // undefined
 
-const testeObjetoCerto = () => ({ nome: 'Juca' })
-console.log(testeObjetoCerto()) // { nome: 'Juca' }
+console.log(somaComValoresPadrao(1)) // 7
+console.log(somaComValoresPadrao()) // 9
+
+const somaArrowFunction = (a = 3, b = 6) => a + b
+
+console.log(somaArrowFunction(1)) // 7
+console.log(somaArrowFunction()) // 9
