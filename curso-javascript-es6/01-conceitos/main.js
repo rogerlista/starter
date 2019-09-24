d329afd0bc0ba6dda6f9d025257df04a5c281656
Rogério <roger.lista@gmail.com>
2019-09-24 05:48:32 -0300

    Conceitos - classes

--- curso-javascript-es6/01-conceitos/main.js
+++ curso-javascript-es6/01-conceitos/main.js
@@ -1,1 +1,16 @@
-alert('testando...')
+class TodoList {
+  constructor() {
+    this.todos = []
+  }
+
+  addTodo() {
+    this.todos.push('Novo todo')
+    console.log(this.todos)
+  }
+}
+
+let minhaLista = new TodoList()
+
+document.getElementById('novo-todo').onclick = function() {
+  minhaLista.addTodo()
+}
