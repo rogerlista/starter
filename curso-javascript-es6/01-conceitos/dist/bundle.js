d5912e5b5821a32ff4d46f995bb5f8e252006c4e
Rogério <roger.lista@gmail.com>
2019-09-24 07:16:24 -0300

    Conceitos - arrow functions

--- curso-javascript-es6/01-conceitos/dist/bundle.js
+++ curso-javascript-es6/01-conceitos/dist/bundle.js
@@ -10,7 +10,7 @@ console.log(newArr); // [1, 4, 6, 8, 12, 15]
 
 var sum = arr.reduce(function (total, next) {
   return total + next;
-}, 0); // não informando o valor inicial de total ele será 0 por padrão.
+}); // não informando o valor inicial de total ele será 0 por padrão.
 
 console.log(sum); // 31
 // retorna um novo array retirando os elementos que forem falsos de acordo com a condição declarada.
@@ -30,3 +30,43 @@ var notFind = arr.find(function (item) {
   return item === 2;
 });
 console.log(notFind); // indefined
+// NÃO RECOMENDADO
+
+var teste = function teste() {
+  return 'teste';
+};
+
+console.log(teste()); // teste
+
+var testeArray = function testeArray() {
+  return [1, 2, 3];
+};
+
+console.log(testeArray()); // [1, 2, 3]
+
+var testeString = function testeString() {
+  return 'Teste';
+};
+
+console.log(testeString()); // Teste
+
+var testeNumber = function testeNumber() {
+  return 3;
+};
+
+console.log(testeNumber()); // 3
+// não vai funcionar entende que seja o corpo da função
+
+var testeObjeto = function testeObjeto() {
+  nome: 'Juca';
+};
+
+console.log(testeObjeto()); // undefined
+
+var testeObjetoCerto = function testeObjetoCerto() {
+  return {
+    nome: 'Juca'
+  };
+};
+
+console.log(testeObjetoCerto()); // { nome: 'Juca' }
--- curso-javascript-es6/01-conceitos/main.js
+++ curso-javascript-es6/01-conceitos/main.js
@@ -1,35 +1,45 @@
 const arr = [1, 3, 4, 5, 8, 10]
 
 // retorna um novo array fazendo alguma coisa com os elementos do array atual.
-const newArr = arr.map(function(item, index) {
-  return item + index
-})
-
+const newArr = arr.map((item, index) => item + index)
 console.log(newArr) // [1, 4, 6, 8, 12, 15]
 
 // retorna um único valor fazendo alguma coisa com os elementos do array atual.
-const sum = arr.reduce(function(total, next) {
-  return total + next
-}, 0) // não informando o valor inicial de total ele será 0 por padrão.
-
+const sum = arr.reduce((total, next) => total + next) // não informando o valor inicial de total ele será 0 por padrão.
 console.log(sum) // 31
 
 // retorna um novo array retirando os elementos que forem falsos de acordo com a condição declarada.
-const filter = arr.filter(function(item) {
-  return item % 2 === 0
-})
-
+const filter = arr.filter(item => item % 2 === 0)
 console.log(filter) // [4, 8, 10]
 
 // retorna o elemento se encontrar ou indefined.
-const find = arr.find(function(item) {
-  return item === 4
-})
-
+const find = arr.find(item => item === 4)
 console.log(find) // 4
 
-const notFind = arr.find(function(item) {
-  return item === 2
-})
-
+const notFind = arr.find(item => item === 2)
 console.log(notFind) // indefined
+
+// NÃO RECOMENDADO
+const teste = () => {
+  return 'teste'
+}
+
+console.log(teste()) // teste
+
+const testeArray = () => [1, 2, 3]
+console.log(testeArray()) // [1, 2, 3]
+
+const testeString = () => 'Teste'
+console.log(testeString()) // Teste
+
+const testeNumber = () => 3
+console.log(testeNumber()) // 3
+
+// não vai funcionar entende que seja o corpo da função
+const testeObjeto = () => {
+  nome: 'Juca'
+}
+console.log(testeObjeto()) // undefined
+
+const testeObjetoCerto = () => ({ nome: 'Juca' })
+console.log(testeObjetoCerto()) // { nome: 'Juca' }
