844fdf55f63b03a136cc1e2f378b7effa96b8820
Rogério <roger.lista@gmail.com>
2019-09-25 06:07:30 -0300

    Conceitos - template literals

--- curso-javascript-es6/01-conceitos/dist/bundle.js
+++ curso-javascript-es6/01-conceitos/dist/bundle.js
@@ -1,24 +1,6 @@
 "use strict";
 
-function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
-
-function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(source, true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(source).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
-
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
-
-var arr1 = [1, 2, 3];
-var arr2 = [4, 5, 6];
-var arr3 = [].concat(arr1, arr2);
-console.log(arr3); // [1, 2, 3, 4, 5, 6]
-
-var usuario = {
-  nome: 'Juca',
-  idade: 25,
-  empresa: 'Rocketseat'
-};
-
-var usuario2 = _objectSpread({}, usuario, {
-  nome: 'Tabajara'
-});
-
-console.log(usuario2); // { nome: 'Tabajara', idade: 25, empresa: 'Rocketseat'}
+var nome = 'Juca';
+var idade = 25;
+console.log('Meu nome é ' + nome + ' e tenho ' + idade + ' anos.');
+console.log("Meu nome \xE9 ".concat(nome, " e tenho ").concat(idade, " anos."));
--- curso-javascript-es6/01-conceitos/main.js
+++ curso-javascript-es6/01-conceitos/main.js
@@ -1,16 +1,6 @@
-const arr1 = [1, 2, 3]
-const arr2 = [4, 5, 6]
+const nome = 'Juca'
+const idade = 25
 
-const arr3 = [...arr1, ...arr2]
+console.log('Meu nome é ' + nome + ' e tenho ' + idade + ' anos.')
 
-console.log(arr3) // [1, 2, 3, 4, 5, 6]
-
-const usuario = {
-  nome: 'Juca',
-  idade: 25,
-  empresa: 'Rocketseat'
-}
-
-const usuario2 = { ...usuario, nome: 'Tabajara' }
-
-console.log(usuario2) // { nome: 'Tabajara', idade: 25, empresa: 'Rocketseat'}
+console.log(`Meu nome é ${nome} e tenho ${idade} anos.`)
