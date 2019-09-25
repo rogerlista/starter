cb8a18d2274370771b6a9c3d287286543c01870d
Rogério <roger.lista@gmail.com>
2019-09-25 11:06:14 -0300

    Desafio - exercício 6 ES6 - template literals

---
+++ curso-javascript-es6/01-conceitos/desafio/main6.js
@@ -0,0 +1,6 @@
+const usuario = 'Juca'
+const idade = 25
+
+console.log('O usuário ' + usuario + ' possui ' + idade + ' anos.') // O usuário Juca possui 25 anos.'
+
+console.log(`O usuário ${usuario} possui ${idade} anos.`) // O usuário Juca possui 25 anos.'
