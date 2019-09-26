af69e9a51be331d8ba4011bf65168c460bd3ca8b
Rogério <roger.lista@gmail.com>
2019-09-26 06:51:54 -0300

    Desafio - exercício 8 Webpack - import/export

--- curso-javascript-es6/01-conceitos/src/main.js
+++ curso-javascript-es6/01-conceitos/src/main.js
@@ -1,1 +1,10 @@
-alert('Testando...')
+import ClasseUsuario from './usuario'
+import { idade } from './usuario'
+
+import Usuario, { idade as idadeUsuario } from './usuario'
+
+ClasseUsuario.info() // Apenas teste
+console.log(idade) // 25
+
+Usuario.info() // Apenas teste
+console.log(idadeUsuario) // 25
---
+++ curso-javascript-es6/01-conceitos/src/usuario.js
@@ -0,0 +1,7 @@
+export const idade = 25
+
+export default class Usuario {
+  static info() {
+    console.log('Apenas teste')
+  }
+}
