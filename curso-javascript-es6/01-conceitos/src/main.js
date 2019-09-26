c23eef686aa581cfee8e9ce2e533b8aab469661d
Rogério <roger.lista@gmail.com>
2019-09-26 07:26:16 -0300

    Trabalhando com async/await

--- curso-javascript-es6/01-conceitos/src/main.js
+++ curso-javascript-es6/01-conceitos/src/main.js
@@ -1,10 +1,59 @@
-import ClasseUsuario from './usuario'
-import { idade } from './usuario'
+function minhaPromise() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('OK')
+    }, 2000)
+  })
+}
 
-import Usuario, { idade as idadeUsuario } from './usuario'
+minhaPromise()
+  .then(response => console.log(response)) // OK
+  .catch(err => console.log(err))
 
-ClasseUsuario.info() // Apenas teste
-console.log(idade) // 25
+async function executaPromise() {
+  const response = await minhaPromise()
+  console.log(response)
+}
 
-Usuario.info() // Apenas teste
-console.log(idadeUsuario) // 25
+executaPromise() // OK
+
+async function executaPromise1() {
+  console.log('Async', await minhaPromise())
+  console.log('Async', await minhaPromise())
+  console.log('Async', await minhaPromise())
+  console.log('Async', await minhaPromise())
+  console.log('Async', await minhaPromise())
+}
+
+executaPromise1() // a cada 2 segundos será executada cada uma das linhas da função exibindo OK.
+
+// o mesmo exemplo que o de cima mas utilizando then
+minhaPromise().then(res => {
+  console.log('then', res)
+
+  minhaPromise().then(res => {
+    console.log('then', res)
+
+    minhaPromise().then(res => {
+      console.log('then', res)
+
+      minhaPromise().then(res => {
+        console.log('then', res)
+
+        minhaPromise().then(res => {
+          console.log('then', res)
+        })
+      })
+    })
+  })
+})
+
+const executaPromiseArrow = async () => {
+  console.log('Arrow', await minhaPromise())
+  console.log('Arrow', await minhaPromise())
+  console.log('Arrow', await minhaPromise())
+  console.log('Arrow', await minhaPromise())
+  console.log('Arrow', await minhaPromise())
+}
+
+executaPromiseArrow()
