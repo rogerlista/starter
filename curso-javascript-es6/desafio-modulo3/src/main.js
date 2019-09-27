42593d0ffa3cc1f706a6fd7163db0889d66a45e8
Rogério <roger.lista@gmail.com>
2019-09-27 10:47:30 -0300

    Desafio módulo 3 ES6 - exercício 2

--- curso-javascript-es6/desafio-modulo3/src/main.js
+++ curso-javascript-es6/desafio-modulo3/src/main.js
@@ -29,3 +29,31 @@ async function umPorSegundoAwait() {
 }
 
 umPorSegundoAwait()
+
+import axios from 'axios'
+
+function getUserFromGithub(user) {
+  axios
+    .get(`https://api.github.com/users/${user}`)
+    .then(response => {
+      console.log(response.data)
+    })
+    .catch(err => {
+      console.log('Usuário não existe.')
+    })
+}
+
+getUserFromGithub('rogerlista')
+getUserFromGithub('rogerlistadkfjdsklfjdkls')
+
+async function getUserFromGithubWait(user) {
+  try {
+    const response = await axios.get(`https://api.github.com/users/${user}`)
+    console.log(response.data)
+  } catch (err) {
+    console.log('Usuário não localizado.')
+  }
+}
+
+getUserFromGithubWait('rogerlista')
+getUserFromGithubWait('rogerlistadkjfkdlsfj')
