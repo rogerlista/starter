a8eab0ff1c250063228e66abad726c6a16323f2e
Rogério <roger.lista@gmail.com>
2019-09-27 10:28:46 -0300

    Desfio módulo 3 ES6 - exercíco 1

--- curso-javascript-es6/desafio-modulo3/src/main.js
+++ curso-javascript-es6/desafio-modulo3/src/main.js
@@ -1,1 +1,31 @@
-alert('Testando...')
+// função delay aciona o .then após 1s
+const delay = () => new Promise(resolve => setTimeout(resolve, 1000))
+
+function umPorSegundo() {
+  delay().then(() => {
+    console.log('1s')
+
+    delay().then(() => {
+      console.log('2s')
+
+      delay().then(() => {
+        console.log('3s')
+      })
+    })
+  })
+}
+
+umPorSegundo()
+
+async function umPorSegundoAwait() {
+  await delay()
+  console.log('await 1s')
+
+  await delay()
+  console.log('await 2s')
+
+  await delay()
+  console.log('await 3s')
+}
+
+umPorSegundoAwait()
