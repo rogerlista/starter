fa34cab907dcd8fdd13f288af823c777a54053fb
Rogério <roger.lista@gmail.com>
2019-09-27 21:29:57 -0300

    Aplicação com ES6+ - renderizando em tela

--- curso-javascript-es6/aplicacao-es6/src/main.js
+++ curso-javascript-es6/aplicacao-es6/src/main.js
@@ -2,6 +2,7 @@ class App {
   constructor() {
     this.repositories = []
     this.formEl = document.getElementById('repo-form')
+    this.ulEl = document.getElementById('repo-list')
 
     this.registerHandle()
   }
@@ -21,7 +22,38 @@ class App {
     }
 
     this.repositories.push(repository)
-    console.log(this.repositories)
+    this.render()
+  }
+
+  render() {
+    this.ulEl.innerHTML = ''
+
+    this.repositories.forEach(repository => {
+      const imgEl = document.createElement('img')
+      imgEl.setAttribute('src', repository.avatar_url)
+
+      const strongEl = document.createElement('strong')
+      strongEl.appendChild(document.createTextNode(repository.name))
+
+      const pEl = document.createElement('p')
+      pEl.appendChild(document.createTextNode(repository.description))
+
+      const aEl = document.createElement('a')
+      aEl.setAttribute('target', '_blank')
+      aEl.setAttribute('href', repository.html_url)
+      aEl.appendChild(document.createTextNode('Acessar'))
+
+      const divEl = document.createElement('div')
+      divEl.appendChild(strongEl)
+      divEl.appendChild(pEl)
+      divEl.appendChild(aEl)
+
+      const liEl = document.createElement('li')
+      liEl.appendChild(imgEl)
+      liEl.appendChild(divEl)
+
+      this.ulEl.appendChild(liEl)
+    })
   }
 }
 
