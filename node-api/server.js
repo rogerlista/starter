8e8fbe41342cca0e02a78d8711c7a147c4c322b5
Rogério <roger.lista@gmail.com>
2019-09-28 07:21:22 -0300

    NodeJS - conectando ao banco de dados MongoDB

--- node-api/server.js
+++ node-api/server.js
@@ -1,6 +1,12 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
+
+mongoose.connect('mongodb://localhost:27017/nodeapi', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
 
 app.get('/', (req, res) => {
   res.send('Hello World')
