4d41f8a8aa7e1ab27980d298abca56ea39488334
Rogério <roger.lista@gmail.com>
2019-09-28 07:36:56 -0300

    NodeJS - criando o model de produto

--- node-api/server.js
+++ node-api/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const requireDir = require('require-dir')
 
 const app = express()
 
@@ -8,8 +9,18 @@ mongoose.connect('mongodb://localhost:27017/nodeapi', {
   useUnifiedTopology: true
 })
 
+requireDir('./src/models')
+
+const Product = mongoose.model('Product')
+
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  Product.create({
+    title: 'React Native',
+    description: 'Build native apps with React',
+    url: 'http://github.com/facebook/react-native'
+  })
+
+  return res.send('Hello World')
 })
 
 app.listen(3001)
