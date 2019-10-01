9794d23b206a5497664575eeb4de67a21f2ac66a
Rogério <roger.lista@gmail.com>
2019-10-01 06:41:29 -0300

    ReadJS - buscando produtos da API

--- huntweb/src/App.js
+++ huntweb/src/App.js
@@ -3,11 +3,13 @@ import React from 'react'
 import './styles.css'
 
 import Header from './components/header'
+import Main from './pages/main'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Main />
     </div>
   )
 }
