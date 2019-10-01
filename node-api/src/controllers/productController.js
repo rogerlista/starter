1ab62d7eb76eac3072cab7e60993dbf94c721a03
Rogério <roger.lista@gmail.com>
2019-09-30 21:19:09 -0300

    NodeJS - implementação do CRUD

--- node-api/src/controllers/productController.js
+++ node-api/src/controllers/productController.js
@@ -13,5 +13,25 @@ module.exports = {
     const product = await Product.create(req.body)
 
     return res.json(product)
+  },
+
+  async show(req, res) {
+    const product = await Product.findById(req.params.id)
+
+    return res.json(product)
+  },
+
+  async update(req, res) {
+    const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+      new: true
+    })
+
+    return res.json(product)
+  },
+
+  async destroy(req, res) {
+    await Product.findByIdAndRemove(req.params.id)
+
+    return res.send()
   }
 }
--- node-api/src/routes.js
+++ node-api/src/routes.js
@@ -6,5 +6,8 @@ const productController = require('./controllers/productController')
 
 routes.get('/products', productController.index)
 routes.post('/products', productController.store)
+routes.get('/products/:id', productController.show)
+routes.put('/products/:id', productController.update)
+routes.delete('/products/:id', productController.destroy)
 
 module.exports = routes
