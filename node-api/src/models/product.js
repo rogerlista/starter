22b0ee4886b1db102b3f11adb4a4c7b35ec7944a
Rogério <roger.lista@gmail.com>
2019-09-30 21:38:30 -0300

    NodeJS - paginação da lista

--- node-api/src/models/product.js
+++ node-api/src/models/product.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const mongoosePaginate = require('mongoose-paginate')
 
 const ProductSchema = new mongoose.Schema({
   title: {
@@ -21,5 +22,7 @@ const ProductSchema = new mongoose.Schema({
     default: Date.now
   }
 })
+
+ProductSchema.plugin(mongoosePaginate)
 
 mongoose.model('Product', ProductSchema)
