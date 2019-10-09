8237c8d6619d05579b60b0a3535765b032f7f932
Rogério <roger.lista@gmail.com>
2019-10-09 20:38:24 -0300

    Hunt mobile - estilizando header e statusbar

--- hunt/src/index.js
+++ hunt/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Routes from './routes';
 
+import './config/status-bar-config';
+
 export default function App() {
   return <Routes />;
 }
--- hunt/src/routes.js
+++ hunt/src/routes.js
@@ -4,9 +4,19 @@ import {createStackNavigator} from 'react-navigation-stack';
 import Main from './pages/main';
 
 const Routes = createAppContainer(
-  createStackNavigator({
-    Main,
-  }),
+  createStackNavigator(
+    {
+      Main,
+    },
+    {
+      defaultNavigationOptions: {
+        headerStyle: {
+          backgroundColor: '#da552f',
+        },
+        headerTintColor: '#fff',
+      },
+    },
+  ),
 );
 
 export default Routes;
