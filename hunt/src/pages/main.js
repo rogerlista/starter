a4caf59f1cb49c710c8e99d7c723887b8f137dbe
Rogério <roger.lista@gmail.com>
2019-10-09 20:55:44 -0300

    Hunt mobile - buscando produtos da API

--- hunt/src/pages/main.js
+++ hunt/src/pages/main.js
@@ -1,8 +1,20 @@
-import React from 'react';
-
+import React, {useEffect} from 'react';
 import {View, Text} from 'react-native';
 
+import api from '../services/api';
+
 function Main() {
+  useEffect(() => {
+    loadProducts();
+  });
+
+  async function loadProducts() {
+    const response = await api.get('/products');
+    const {docs} = response.data;
+
+    console.log(docs);
+  }
+
   return (
     <View>
       <Text>Página principal</Text>
