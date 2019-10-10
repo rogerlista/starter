e3033198ca35667a19cf339427f6ada6217ce635
Rogério <roger.lista@gmail.com>
2019-10-09 21:05:27 -0300

    Hunt mobile - lidando com os estados dos componentes

--- hunt/src/pages/main.js
+++ hunt/src/pages/main.js
@@ -1,23 +1,29 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {View, Text} from 'react-native';
 
 import api from '../services/api';
 
 function Main() {
+  const [count, setCount] = useState(0);
+  const [docs, setDocs] = useState([]);
+
   useEffect(() => {
     loadProducts();
-  });
+  }, []);
 
   async function loadProducts() {
     const response = await api.get('/products');
     const {docs} = response.data;
 
-    console.log(docs);
+    setDocs(docs);
   }
 
   return (
     <View>
       <Text>Página principal</Text>
+      {docs.map(product => (
+        <Text key={product._id}>{product.title}</Text>
+      ))}
     </View>
   );
 }
