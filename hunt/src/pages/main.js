67cdb589c4f14e66054ab51f6b2c5d9e9837c945
Rogério <roger.lista@gmail.com>
2019-10-09 21:28:49 -0300

    Hunt mobile - estilizando lista de produtos

--- hunt/src/pages/main.js
+++ hunt/src/pages/main.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import {View, Text, FlatList, TouchableOpacity} from 'react-native';
+import {View, Text, FlatList, TouchableOpacity, StyleSheet} from 'react-native';
 
 import api from '../services/api';
 
@@ -20,20 +20,21 @@ function Main() {
 
   function renderItem({item}) {
     return (
-      <View>
-        <Text>{item.title}</Text>
-        <Text>{item.description}</Text>
+      <View style={styles.productContainer}>
+        <Text style={styles.productTitle}>{item.title}</Text>
+        <Text style={styles.productDescription}>{item.description}</Text>
 
-        <TouchableOpacity onPress={() => {}}>
-          <Text>Acessar</Text>
+        <TouchableOpacity style={styles.productButton} onPress={() => {}}>
+          <Text style={styles.productButtonText}>Acessar</Text>
         </TouchableOpacity>
       </View>
     );
   }
 
   return (
-    <View>
+    <View style={styles.container}>
       <FlatList
+        contentContainerStyle={styles.list}
         data={docs}
         keyExtractor={item => item._id}
         renderItem={renderItem}
@@ -45,5 +46,55 @@ function Main() {
 Main.navigationOptions = {
   title: 'Main page',
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fafafa',
+  },
+
+  list: {
+    padding: 20,
+  },
+
+  productContainer: {
+    backgroundColor: '#fff',
+    borderWidth: 1,
+    borderColor: '#ddd',
+    borderRadius: 5,
+    padding: 20,
+    marginBottom: 20,
+  },
+
+  productTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+
+  productDescription: {
+    fontSize: 16,
+    color: '#999',
+    marginTop: 5,
+    lineHeight: 24,
+  },
+
+  productButton: {
+    height: 42,
+    borderRadius: 5,
+    borderWidth: 2,
+    borderColor: '#da552f',
+    backgroundColor: 'transparent',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 10,
+  },
+
+  productButtonText: {
+    fontSize: 16,
+    color: '#da552f',
+    fontWeight: 'bold',
+  },
+});
 
 export default Main;
