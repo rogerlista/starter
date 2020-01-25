76870897d03fc310d97f6550bd4c34f309647f86
Rogério <roger.lista@gmail.com>
2020-01-25 11:43:28 -0300

    Iniciando a aplicação Todo

--- curso-javascript/03-app-todos/index.js
+++ curso-javascript/03-app-todos/index.js
@@ -1,1 +1,9 @@
-alert("Funcionou");
+const listElement = document.querySelector("ul");
+const inputElement = document.querySelector("input");
+const buttonElement = document.querySelector("button");
+
+const list = [
+  "Fazer café",
+  "Estudar Javascript",
+  "Entrar na comunidade da Rocketseat"
+];
