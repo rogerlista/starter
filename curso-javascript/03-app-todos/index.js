504589d88ede57ab237e61b578ddf97f59804a62
Rogério <roger.lista@gmail.com>
2020-01-25 11:48:23 -0300

    Renderizando Todos

--- curso-javascript/03-app-todos/index.js
+++ curso-javascript/03-app-todos/index.js
@@ -2,8 +2,20 @@ const listElement = document.querySelector("ul");
 const inputElement = document.querySelector("input");
 const buttonElement = document.querySelector("button");
 
-const list = [
+const todos = [
   "Fazer café",
   "Estudar Javascript",
   "Entrar na comunidade da Rocketseat"
 ];
+
+function renderTodos() {
+  for (todo of todos) {
+    const todoElement = document.createElement("li");
+    const textElement = document.createTextNode(todo);
+
+    todoElement.appendChild(textElement);
+    listElement.appendChild(todoElement);
+  }
+}
+
+renderTodos();
