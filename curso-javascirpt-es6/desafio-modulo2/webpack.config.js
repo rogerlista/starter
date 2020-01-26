2a8e1d25f122ea8708ed0e901a006ba63d3293e6
Rogério <roger.lista@gmail.com>
2020-01-26 19:36:29 -0300

    Desafio módulo 02 webpack server

---
+++ curso-javascirpt-es6/desafio-modulo2/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+}
