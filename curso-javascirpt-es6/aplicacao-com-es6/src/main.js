eb87f849fa23cd218fe7580490aab95f93f12afc
Rogério <roger.lista@gmail.com>
2020-01-26 21:57:24 -0300

    Renderizando repositorios na tela

--- curso-javascirpt-es6/aplicacao-com-es6/src/main.js
+++ curso-javascirpt-es6/aplicacao-com-es6/src/main.js
@@ -3,6 +3,7 @@ class App {
     this.repositories = []
 
     this.formElement = document.querySelector('#repo-form')
+    this.listElement = document.querySelector('#repo-list')
 
     this.registerHandle()
   }
@@ -21,7 +22,35 @@ class App {
       html_url: 'http://rocketseat.com.br'
     })
 
-    console.log(this.repositories)
+    this.render()
+  }
+
+  render() {
+    this.listElement.innerHTML = ''
+
+    this.repositories.forEach(repo => {
+      let imgEl = document.createElement('img')
+      imgEl.setAttribute('src', repo.avatar_url)
+      imgEl.setAttribute('alt', 'Logo')
+
+      let titleEl = document.createElement('strong')
+      titleEl.appendChild(document.createTextNode(repo.name))
+
+      let descriptionEl = document.createElement('p')
+      descriptionEl.appendChild(document.createTextNode(repo.description))
+
+      let linkEl = document.createElement('a')
+      linkEl.setAttribute('target', '_blank')
+      linkEl.appendChild(document.createTextNode('Acessar'))
+
+      let listItemEl = document.createElement('li')
+      listItemEl.appendChild(imgEl)
+      listItemEl.appendChild(titleEl)
+      listItemEl.appendChild(descriptionEl)
+      listItemEl.appendChild(linkEl)
+
+      this.listElement.appendChild(listItemEl)
+    })
   }
 }
 
